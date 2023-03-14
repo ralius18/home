20d0d1d1e0a221e2faa0c3257e2cbe92a3b4adf4
Brad Stone <brad.stone@re-leased.com>
2023-03-14 16:06:04 +1300

    Add build

---
+++ docs/js/about.609fdd43.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkhome_vue"]=self["webpackChunkhome_vue"]||[]).push([[443],{6897:function(u,e,n){n.r(e),n.d(e,{default:function(){return i}});var t=function(){var u=this;u._self._c;return u._m(0)},s=[function(){var u=this,e=u._self._c;return e("div",{staticClass:"about"},[e("h1",[u._v("This is an about page")])])}],a=n(1001),l={},r=(0,a.Z)(l,t,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.609fdd43.js.map
