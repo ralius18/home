c9aabb0825403c7e558a451ac7d1b127d9783ffb
ralius18 <brayds97@gmail.com>
2024-04-18 16:16:02 +1200

    Update CV

--- src/models/job.ts
+++ src/models/job.ts
@@ -3,15 +3,17 @@
   title: string
   company: string
   dates: string[]
+  duration: string
   website: string
   companyDescription: string
   description: string
 
-  constructor(detailsVisible: boolean, title: string, company: string, dates: string[], website: string, companyDescription: string, description: string ) {
+  constructor(detailsVisible: boolean, title: string, company: string, dates: string[], duration: string, website: string, companyDescription: string, description: string ) {
     this.detailsVisible = detailsVisible
     this.title = title
     this.company = company
     this.dates = dates
+    this.duration = duration
     this.website = website
     this.companyDescription = companyDescription
     this.description = description
--- src/views/WorkSection.vue
+++ src/views/WorkSection.vue
@@ -9,7 +9,7 @@
         {{ job.title }}&nbsp;-&nbsp;<a :href="job.website" target="_blank">{{ job.company }}</a><v-icon small class="ml-2">mdi-open-in-new</v-icon>
       </v-card-title>
       <v-card-subtitle>
-        {{ job.dates[0] }} - {{ job.dates[1] }}
+        {{ job.dates[0] }} - {{ job.dates[1] }} ({{ job.duration }})
       </v-card-subtitle>
       <v-card-text v-show="job.detailsVisible">
         <v-row class="mb-4">
@@ -35,9 +35,20 @@ export default Vue.extend({
       jobs: [
         {
           detailsVisible: true,
+          title: 'Junior Data Technician',
+          company: 'Cablenet',
+          dates: ['December 2023', 'April 2024'],
+          duration: '5 months',
+          website: 'https://advancedgroup.co.nz/cablenet-home/',
+          companyDescription: 'Cablenet is part of the wider Advanced Group specialising in data cable installation for commercial customers, covering all types of ICT projects, from time critical nationwide technical rollouts, to large scale data, wireless, nurse call systems, audio visual and electrical contracts.',
+          description: 'This role interested me to learn more about the physical side of networking and gain an understanding of the process behind installing infrastructure.I started as a junior without knowing much about the job but learned very quickly to be able to run cables, fit off fixtures and other small jobs by myself as well as assisting senior technicians in the more complicated tasks'
+        },
+        {
+          detailsVisible: true,
           title: 'Software Developer',
           company: 'Re-Leased',
           dates: ['June 2021', 'March 2023'],
+          duration: '1 year, 8 months',
           website: 'https://re-leased.com',
           companyDescription: 'Re-Leased is a cloud-based commercial property management software solution. Designed for property owners and managers of both residential and commercial property. With an easy-to-use dashboard and automated workflows, Re-Leased lets customers focus on growing their business whilst mitigating risks associated with managing commercial properties.',
           description: 'I joined as a junior developer and worked my way up to a Full Stack role. This introduced me to benefits such as working in an Agile environment and using Git to its full potential as a development process. This role taught me a lot about working in a larger scale software business and the practices of web development.'
@@ -47,6 +58,7 @@ export default Vue.extend({
           title: 'Software Developer',
           company: 'MAGIQ Software',
           dates: ['January 2019', 'June 2021'],
+          duration: '2 years, 6 months',
           website: 'https://magiqsoftware.com',
           companyDescription: 'MAGIQ provides Cloud finance and business administration software for the Public Sector. The modular based ERP system is used in key markets such as Local Government, Health & Community Services, Utilities, Education and Not for Profit.',
           description: 'This was my first post-university software development role developing software in a professional environment. I learned new languages and gained a more in depth understanding of computing systems from a software perspective. This role mainly consisted of projects requiring integration of third party apps, product development and bug fixing. Due to the system covering broad areas such as financing, regulatory systems and reporting, it required me to learn about a wide range of topics relevant to our main clients of local councils.'
@@ -56,6 +68,7 @@ export default Vue.extend({
           title: 'Technical Support and Sales',
           company: 'NOW NZ',
           dates: ['May 2018 ', 'December 2018'],
+          duration: '6 months',
           website: 'https://nownz.co.nz',
           companyDescription: 'NOW NZ is a New Zealand based broadband and business communication provider who take service seriously with local customer and technical service teams.',
           description: 'This was my starting point in the IT industry after graduating from university. The role comprised of remote-based customer technical support, assisting customers with various technical issues. NOW\'s main point of difference is the customer service provided, so it was extremely advantageous for a technically minded person such as myself to be customer facing and talk people through issues.'
