006762d817bdeb1ad85d860a704cf96f156595a8
ralius18 <brayds97@gmail.com>
2024-07-10 10:19:15 +1200

    Update CV

--- src/views/StatementSection.vue
+++ src/views/StatementSection.vue
@@ -5,7 +5,7 @@
         I am a resourceful and adaptable Intermediate Software Developer with 5 years experience in a wide range of technologies.
         I particularly enjoy working on Front-End and UX development as I thoroughly enjoy seeing the positive impact it can make.
         With most of my experience being as a Full-Stack developer, I am also very comfortable working on Back-End projects as well.
-        As a lifelong learner, I am always looking for ways to improve my skills and bring new ideas and approaches to my work. 
+        As a lifelong learner, I am always looking for ways to improve my skills and bring new ideas and approaches to my work.
         <!-- <br />
         <br />
         I am also very interested in the world of music and audio production.
